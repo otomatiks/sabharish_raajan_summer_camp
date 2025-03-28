89389d3f30669bad7d6c1b5bf0e17caf183a49db
Otoamtiks <otomatiksit@gmail.com>
2025-03-28 13:21:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// document.getElementById('Admission form').addEventListener('submit', function (event) {
+//     event.preventDefault();
+  
+//   // Write your code below
+  
+//     const firstName = document.getElementById('name').value;
+//     const email = document.getElementById('email').value;
+//     // const district = document.getElementById('disrict').value;
+//     const phone = document.getElementById('phone').value;
+//     // const gender = document.getElementById('Gender').value;
+//     const password = document.getElementById('password').value;
+//     const message = document.getElementById('messsage').value;
+//    c
+   
+//     alert(`Your registration was successful!\n\n
+//     Name: ${firstName}\n
+//     Email: ${email}\n
+//     Phoe Number: ${phone}\n
+  
+
+//     Password: ${password}\n
+//    Message: ${message}`);
+//   });
+  
